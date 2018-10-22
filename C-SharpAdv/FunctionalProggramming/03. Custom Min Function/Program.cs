47afb439e954ee7071c400a5fbc2c5204436591b
Borislav Kunchev <gearboxxbg@gmail.com>
2018-10-22 16:28:45 +0300

    Update 22.10
    MDA Solution fix
    Streams Solution fix
    Added Functional programming 1-6

---
+++ C-SharpAdv/FunctionalProggramming/03. Custom Min Function/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] ints = Console.ReadLine()
+				.Split()
+				.Select(int.Parse)
+				.ToArray();
+
+			Func<int[], int> minFunc = arr =>
+			{
+				int min = arr[0];
+				for (int i = 1; i < arr.Length; i++)
+				{
+					if (min > arr[i])
+					{
+						min = arr[i];
+					}
+				}
+				return min;
+			};
+
+			Console.WriteLine(minFunc(ints));
+		}
+	}
