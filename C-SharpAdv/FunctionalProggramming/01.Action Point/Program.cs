71156c59a4ff455c37ef9161cebba0aa0ad2fcdd
Borislav Kunchev <gearboxxbg@gmail.com>
2018-10-24 10:38:37 +0300

    Gitignore
    bin / obj added to gitignore

---
+++ C-SharpAdv/FunctionalProggramming/01.Action Point/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Action<string> print = message => Console.WriteLine(message);
+			List<string> name = Console.ReadLine().Split(' ').ToList();
+
+			foreach (var word in name)
+			{
+				print(word);
+			}
+		}
+	}
---
+++ C-SharpAdv/Streams/02. Line Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _02.Line_Numbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var readStream = new StreamReader(@"C:\Users\Gearboxx\Desktop\repository\SoftUni\C-SharpAdv\Streams\02. Line Numbers\text.txt"))
+			{
+				using (var writeStream = new StreamWriter(@"C:\Users\Gearboxx\Desktop\repository\SoftUni\C-SharpAdv\Streams\02. Line Numbers\newfile.txt"))
+				{
+					int lineNumber = 1;
+					string line;
+
+					while ((line = readStream.ReadLine()) != null)
+					{
+						writeStream.WriteLine($"Line {lineNumber}: " + line);
+						lineNumber++;
+					}
+				}
+			}
+		}
+	}
+}
