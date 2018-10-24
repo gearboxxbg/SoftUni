09e52afb6c879458d7bd1bd58120acc15a73f961
Borislav Kunchev <gearboxxbg@gmail.com>
2018-10-24 15:48:08 +0300

    Stacks and Queues
    exp 6 added

---
+++ C-SharpAdv/Stacks And Queues/06. Truck Tour/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _06._Truck_Tour
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine()), k = 0, sum = 0, m = 0;
+			int[] pumpsInfo = new int[2 * n];
+
+			for (int i = 0; i < n; i++)
+			{
+				int[] pumpInfo = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+				pumpsInfo[i] = pumpInfo[0];
+				pumpsInfo[i + n] = pumpInfo[1];
+			}
+			Queue<int> pumps = new Queue<int>();
+			pumps.Enqueue(0); m++;
+			int amountOfGas = pumpsInfo[0];
+			int distanceToNext = pumpsInfo[n];
+			sum = amountOfGas - distanceToNext;
+
+			for (int i = 1; i <= n + k; i++)
+			{
+				if (sum < 0)
+				{
+					pumps.Clear(); k += m; m = 0;
+					pumps.Enqueue(i);
+					amountOfGas = pumpsInfo[i % n]; distanceToNext = pumpsInfo[i % n + n];
+					sum = amountOfGas - distanceToNext;
+				}
+				else
+				{
+					m++;
+					pumps.Enqueue(i);
+					amountOfGas = pumpsInfo[i % n]; distanceToNext = pumpsInfo[i % n + n];
+					sum += amountOfGas - distanceToNext;
+				}
+			}
+			Console.WriteLine(pumps.Peek());
+		}
+	}
+}
