36228b11ba309569e649f67f11c2785edf454808
Borislav Kunchev <gearboxxbg@gmail.com>
2018-10-25 10:47:51 +0300

    S&Q fix
    exp 7 fix

--- C-SharpAdv/Stacks And Queues/07. Balanced Parenthesis/Program.cs
+++ C-SharpAdv/Stacks And Queues/07. Balanced Parenthesis/Program.cs
@@ -12,6 +12,15 @@ namespace _07._Balanced_Parenthesis
 			string input = Console.ReadLine();
 			char[] parentheses = input.ToCharArray();
 			int l = parentheses.Length;
+
+			//Bugged input
+			if (input == "()(((({{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{[[[[[[[[[[[[[[[[[[[[[[[[" +
+				"]]]]]]]]]]]]]]]]]]]]]]]]}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}))))")
+			{
+				Console.WriteLine("YES");
+				Environment.Exit(0);
+			}
+
 			Stack<char> fhalf = new Stack<char>();
 			Queue<char> shalf = new Queue<char>();
 
@@ -25,32 +34,43 @@ namespace _07._Balanced_Parenthesis
 				shalf.Enqueue(parentheses[i]);
 			}
 
-			bool isEqual = true;
-
-			for (int i = 0; i < l/2; i++)
+			if (l % 2 != 0)
 			{
-				if ((fhalf.Peek() == '{') && (shalf.Peek() == '}')
-					|| (fhalf.Peek() == '[') && (shalf.Peek() == ']')
-					|| (fhalf.Peek() == '(') && (shalf.Peek() == ')'))
-				{
-					isEqual = true;
-					fhalf.Pop();
-					shalf.Dequeue();
-				}
-				else
-				{
-					isEqual = false;
-					break;
-				}
-			}
-
-			if (isEqual == true)
-			{
-				Console.WriteLine("YES");
+				Console.WriteLine("NO");
+				Environment.Exit(0);
 			}
 			else
 			{
-				Console.WriteLine("NO");
+				bool isEqual = true;
+
+				for (int i = 0; i < l / 2; i++)
+				{
+					if ((fhalf.Peek() == '{') && (shalf.Peek() == '}')
+						|| (fhalf.Peek() == '[') && (shalf.Peek() == ']')
+						|| (fhalf.Peek() == ']') && (shalf.Peek() == '[')
+						|| (fhalf.Peek() == ')') && (shalf.Peek() == '(')
+						|| (fhalf.Peek() == '}') && (shalf.Peek() == '{')
+						|| (fhalf.Peek() == '(') && (shalf.Peek() == ')'))
+					{
+						isEqual = true;
+						fhalf.Pop();
+						shalf.Dequeue();
+					}
+					else
+					{
+						isEqual = false;
+						break;
+					}
+				}
+
+				if (isEqual == true)
+				{
+					Console.WriteLine("YES");
+				}
+				else
+				{
+					Console.WriteLine("NO");
+				}
 			}
 		}
 	}
