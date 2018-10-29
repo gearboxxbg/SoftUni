583ad1ccafb864086b314b1f77aaa58a55b3e94b
Borislav Kunchev <gearboxxbg@gmail.com>
2018-10-29 13:37:49 +0200

    Stacks And Queues
    exp 8, 9, 10 added

---
+++ C-SharpAdv/Stacks And Queues/08. Stack Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08._Stack_Fibonacci
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			Stack<long> fibb = new Stack<long>();
+			fibb.Push(0);
+			fibb.Push(1);
+
+			for (int i = 0; i < n - 1; i++)
+			{
+				long x = fibb.Peek();
+				fibb.Pop();
+				long y = fibb.Peek();
+
+				fibb.Push(x);
+				fibb.Push(x + y);
+			}
+
+			Console.WriteLine(fibb.Peek());
+		}
+	}
+}
---
+++ C-SharpAdv/Stacks And Queues/09. Simple Text Editor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace _09._Simple_Text_Editor
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int commandsNum = int.Parse(Console.ReadLine());
+			string text = string.Empty;
+			Stack<string> saved = new Stack<string>();
+
+			for (int i = 0; i < commandsNum; i++)
+			{
+				string[] command = Console.ReadLine().Split();
+				int commandType = int.Parse(command[0]);
+
+				switch (commandType)
+				{
+					case 1:
+						saved.Push(text);
+						string textToAdd = command[1];
+						text += textToAdd;
+						break;
+					case 2:
+						saved.Push(text);
+						int removeIndexes = int.Parse(command[1]);
+						text = text.Remove(text.Length - removeIndexes,removeIndexes);
+						break;
+					case 3:
+						int index = int.Parse(command[1]);
+						Console.WriteLine(text[index - 1]);
+						break;
+					case 4:
+						text = String.Empty;
+						text = saved.Pop();
+						break;
+				}
+			}
+		}
+	}
+}
